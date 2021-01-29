a87cce1badb80bbaa1b45b4c7111fd6f6a2ce128
Ruby Faucher <ruby.faucher@etud.univ-jfc.fr>
2021-01-29 17:06:40 +0100

    Ajout boutton, barre de menu, calendrier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from  '@angular/material/datepicker'; 
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import {MatGridListModule} from '@angular/material/grid-list'; 
 
 @NgModule({
   declarations: [
@@ -12,7 +22,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
