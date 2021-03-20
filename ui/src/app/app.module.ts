d8af0d258d0a8a0ad45f3b7ca7845a2fa9af7042
Ruby Faucher <ruby.faucher@etud.univ-jfc.fr>
2021-03-20 11:32:08 +0100

    Ajout informations vaccins dans Ajouter un vaccin

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,35 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatCardModule } from '@angular/material/card';
-import { MatStepperModule } from '@angular/material/stepper';
-import {MatDialogModule} from '@angular/material/dialog'; 
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field'; 
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatCardModule } from "@angular/material/card";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { STEPPER_GLOBAL_OPTIONS } from "@angular/cdk/stepper";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
-import { CalendrierComponent } from './components/calendrier/calendrier.component';
-import { ChronologieComponent } from './components/chronologie/chronologie.component';
-import { DocumentsComponent } from './components/documents/documents.component';
-import { DialogRappelComponent } from './dialog-rappel/dialog-rappel.component';
-import { AjoutVaccinComponent } from './components/ajout-vaccin/ajout-vaccin.component';
-import { DialogueAjoutVaccComponent } from './components/dialogue-ajout-vacc/dialogue-ajout-vacc.component';
-
-
-
-
+import { CalendrierComponent } from "./components/calendrier/calendrier.component";
+import { ChronologieComponent } from "./components/chronologie/chronologie.component";
+import { DocumentsComponent } from "./components/documents/documents.component";
+import { DialogRappelComponent } from "./dialog-rappel/dialog-rappel.component";
+import { AjoutVaccinComponent } from "./components/ajout-vaccin/ajout-vaccin.component";
+import { DialogueAjoutVaccComponent } from "./components/dialogue-ajout-vacc/dialogue-ajout-vacc.component";
 
 @NgModule({
   declarations: [
@@ -39,7 +37,7 @@ import { DialogueAjoutVaccComponent } from './components/dialogue-ajout-vacc/dia
     DocumentsComponent,
     DialogRappelComponent,
     AjoutVaccinComponent,
-    DialogueAjoutVaccComponent
+    DialogueAjoutVaccComponent,
   ],
   imports: [
     BrowserModule,
@@ -57,13 +55,17 @@ import { DialogueAjoutVaccComponent } from './components/dialogue-ajout-vacc/dia
     MatDialogModule,
     MatDatepickerModule,
     MatFormFieldModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatInputModule,
+    MatTooltipModule,
   ],
-  providers: [{
-    provide: STEPPER_GLOBAL_OPTIONS,
-    useValue: { displayDefaultIndicatorType: false }
-  }],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { displayDefaultIndicatorType: false },
+    },
+  ],
   bootstrap: [AppComponent],
-  entryComponents: [DialogRappelComponent]
+  entryComponents: [DialogRappelComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- ui/src/app/components/ajout-vaccin/ajout-vaccin.component.ts
+++ ui/src/app/components/ajout-vaccin/ajout-vaccin.component.ts
@@ -1,21 +1,20 @@
-import { Vaccin } from './../../model/vaccin';
-import { VaccinsService } from './../../services/vaccins.service';
-import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { DialogueAjoutVaccComponent } from 'src/app/components/dialogue-ajout-vacc/dialogue-ajout-vacc.component'
+import { Vaccin } from "./../../model/vaccin";
+import { VaccinsService } from "./../../services/vaccins.service";
+import { Component, OnInit } from "@angular/core";
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { DialogueAjoutVaccComponent } from "src/app/components/dialogue-ajout-vacc/dialogue-ajout-vacc.component";
 @Component({
-  selector: 'app-ajout-vaccin',
-  templateUrl: './ajout-vaccin.component.html',
-  styleUrls: ['./ajout-vaccin.component.css']
+  selector: "app-ajout-vaccin",
+  templateUrl: "./ajout-vaccin.component.html",
+  styleUrls: ["./ajout-vaccin.component.css"],
 })
 export class AjoutVaccinComponent implements OnInit {
-
   vaccins: Array<Vaccin> = [];
 
   constructor(
     private matDialog: MatDialog,
-    private vaccinsService: VaccinsService,
-  ) { }
+    private vaccinsService: VaccinsService
+  ) {}
 
   ngOnInit(): void {
     this.vaccinsService.getAllVaccins().subscribe((data: any) => {
@@ -26,8 +25,8 @@ export class AjoutVaccinComponent implements OnInit {
 
   openDialogajout(): void {
     const dialogRef = this.matDialog.open(DialogueAjoutVaccComponent, {
-      width: '375px',
-      height: '500px'
+      width: "375px",
+      height: "500px",
     });
   }
 
